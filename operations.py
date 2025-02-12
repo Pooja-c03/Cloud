acab7454924750e107328112f694de0cf827d8ae
pooja-c03 <pooja0909.c@gmail.com>
2025-02-12 19:41:59 +0530

    new operation

--- operations.py
+++ operations.py
@@ -1,6 +1,13 @@
 # string concatinate
 
-str1 = input("Enter 1st string: ")
+'''str1 = input("Enter 1st string: ")
 str2 = input("Enter 2nd string: ")
 
-print("Concatinated string is ", str1 + str2)
+print("Concatinated string is ", str1 + str2)'''
+
+# string slicing
+
+str = input("Enter the string: ")
+n1 = int(input("Enter how many aplphabets to be sliced: "))
+
+print("Sliced string is ", str[n1:])
