79ea44e503bad344267e0a59136bd1689272511c
pooja-c03 <pooja0909.c@gmail.com>
2025-02-13 22:04:16 +0530

    new operation

--- operations.py
+++ operations.py
@@ -1,13 +1,21 @@
+# All the basic Python operations
+
+
 # string concatinate
 
 '''str1 = input("Enter 1st string: ")
 str2 = input("Enter 2nd string: ")
 
-print("Concatinated string is ", str1 + str2)'''
+print("Concatinated string is ", str1 + str2)
 
 # string slicing
 
 str = input("Enter the string: ")
 n1 = int(input("Enter how many aplphabets to be sliced: "))
 
-print("Sliced string is ", str[n1:])
+print("Sliced string is ", str[n1:])'''
+
+# string reversing
+
+str = input("Enter string to be reversed: ")
+print("Reversed string is ", str[::-1])
