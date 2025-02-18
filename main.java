b6abc32583afe0da570489b5fbbea6b627a2d8e6
pooja-c03 <pooja0909.c@gmail.com>
2025-02-18 18:57:49 +0530

    starting with java

---
+++ main.java
@@ -0,0 +1,18 @@
+// Starting with Java 
+
+public class main{
+    public static void main(string[] args){
+        system.out.println("Java is independent & secure.");
+    }
+}
+
+// Datatypes & Variables
+
+public class DatatypesEg{
+    public static void main(string[] args){
+        int num = 10;
+        double price = 150.50;
+        char letter = "Hello World";
+        boolean status = true;
+    }
+}
